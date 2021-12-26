0a9c430d27ec7cea7c2c912a07fcbc562a2e2105
Zeavu <abombavaiexplodir@gmail.com>
2021-12-26 17:47:30 +0000

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ module.exports = class SpoilerPlugin extends Plugin {
         
         FluxDispatcher.subscribe("MESSAGE_CREATE", ({ message }) => {
           var msg = message.content.toLowerCase();
-          if(msg.includes(words.ye)) {
+          if(msg.includes("asd")) {
             console.log("Spoiler detected")
           }
         })
