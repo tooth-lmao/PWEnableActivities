69b61353a216a6cc79394ae76b35f9b81d58c1fc
Zeavu <abombavaiexplodir@gmail.com>
2021-12-26 19:40:43 +0000

    Update index.js

--- index.js
+++ index.js
@@ -14,8 +14,36 @@ module.exports = class SpoilerPlugin extends Plugin {
         FluxDispatcher.subscribe("MESSAGE_CREATE", ({ message }) => {
           var msg = message.content.toLowerCase();
           var wordlist = ["spider-man", "spider", "tobey", "andrew"]
-          if(wordlist.includes(msg)) console.log("asdasd")
+          if(wordlist.includes(msg)) {
+            
+            
+            const message = args[0].message;
+						document.getElementById(
+							`chat-messages-${message.id}`
+						).style.display = 'none';
+
+						powercord.api.notices.sendToast('message-hidden', {
+							header: 'Success!',
+							type: 'success',
+							timeout: 10000,
+							content:
+								'The message has been hidden. To bring it back, switch to another channel then switch to this one.',
+							buttons: [
+								{
+									text: 'Dismiss',
+									look: 'filled',
+									size: 'small',
+									onClick: () =>
+										powercord.api.notices.closeToast(
+											'message-hidden'
+										),
+								},
+							],
+						});
+          }
         });
+
+        
     pluginWillUnload(); {
       powercord.api.settings.unregisterSettings(this.entityID)
     }
