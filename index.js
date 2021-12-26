bd46d6e7e88e847ad074a5311667bed1ee324f52
Zeavu <abombavaiexplodir@gmail.com>
2021-12-26 19:12:08 +0000

    qa

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ module.exports = class SpoilerPlugin extends Plugin {
         
         FluxDispatcher.subscribe("MESSAGE_CREATE", ({ message }) => {
           var msg = message.content.toLowerCase();
-          console.log(process.env.wordlist);
+          console.log(process.env.wordlist.split(", "));
           
         })
     pluginWillUnload(); {
